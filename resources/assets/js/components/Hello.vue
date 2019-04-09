40de2c67fde15e43ad95068a906597354acdab3f
zhouxiaofei <mrzhouxiaofei@foxmail.com>
2019-04-09 20:50:12 +0800

    测试

--- resources/assets/js/components/Hello.vue
+++ resources/assets/js/components/Hello.vue
@@ -3,7 +3,7 @@
         <h1>Hello, Larvuent!</h1>
         <el-button @click="visible = true">按钮</el-button>
         <el-dialog :visible.sync="visible">
-            <p>欢迎使用 Element</p>
+            <p>欢迎使用 ElementUI</p>
         </el-dialog>
     </div>
 </template>
